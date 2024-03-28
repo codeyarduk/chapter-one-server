b9b14d0fa01d9d62ad4cc272ded4ee36cc1c7585
davidwrossiter <davewrossiter@gmail.com>
2024-03-28 10:41:46 +0000

    Add cors package and updated package.json

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const app = express();
-
+const cors = require("cors");
 // dotenv.config();
-
+app.use(cors());
+app.use(express.json());
 const OpenAI = require("openai");
 const pdf2html = require("pdf2html");
 const { get } = require("http");
@@ -28,27 +29,27 @@ const upload = multer({ storage: storage });
 app.post("/upload", upload.single("file"), async (req, res) => {
   console.log(req.file);
   const text = await pdf2html.text("uploads/" + req.file.filename);
-  getReview(text);
-  console.log(text);
-  res.send("File uploaded successfully" + text);
+  const review = await getReview(text);
+  //   console.log(text);
+  res.send(review);
 });
 
 async function getReview(text) {
-  const chatCompletion = await openai.chat.completions
-    .create({
-      messages: [
-        {
-          role: "user",
-          content:
-            "Please analyse my resume and give me feedback on where i can improve it, I am applying for a corporate finance role, address me by my first name" +
-            text,
-        },
-      ],
-      model: "gpt-4-turbo-preview",
-    })
-    .then((data) => console.log(data.choices[0].message.content));
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content:
+          "Please analyse my resume and give me feedback on where i can improve it, I am applying for a corporate finance role, address me by my first name, and sign the email off from CodeYard" +
+          text,
+      },
+    ],
+    model: "gpt-4-turbo-preview",
+  });
+  // .then((data) => console.log(data.choices[0].message.content));
+  return chatCompletion.choices[0].message.content;
 
-  console.log(chatCompletion);
+  //   console.log(chatCompletion);
 }
 
 const port = process.env.PORT || 3000;
