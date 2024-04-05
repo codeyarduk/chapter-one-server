8946a7c58a6b0c9d4f9b5ae371de05e9d60cf213
davidwrossiter <davewrossiter@gmail.com>
2024-04-05 23:52:52 +0100

    Add Stripe public key and update store items in payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -9,20 +9,29 @@ router.use(express.json());
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const storeItems = new Map([
-  [1, { priceInCents: 50000, name: "Learn React Today" }],
-  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+  [1, { price_id: "price_1P2JGvBVg7XYyapkZMw1qn0c", name: "Basic", tokens: 1 }],
+  [
+    2,
+    { price_id: "price_1P2JFZBVg7XYyapkFALQCSNw", name: "Standard", tokens: 5 },
+  ],
+  [
+    3,
+    { price_id: "price_1P1T8YBVg7XYyapkulFsFHk2", name: "Premium", tokens: 15 },
+  ],
 ]);
 
 router.post("/create-checkout-session", async (req, res) => {
+  console.log(req.body);
   try {
+    const storeItem = storeItems.get(req.body.item.id);
+    console.log(storeItem);
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      line_items: req.body.items.map((item) => {
-        const storeItem = storeItems.get(item.id);
-        return {
-          price: "price_1P1T8YBVg7XYyapkulFsFHk2",
-          quantity: item.quantity,
+      line_items: [
+        {
+          price: storeItem.price_id,
+          quantity: req.body.item.quantity,
           //   price_data: {
           //     currency: "usd",
           //     product_data: {
@@ -31,13 +40,15 @@ router.post("/create-checkout-session", async (req, res) => {
           //     unit_amount: storeItem.priceInCents,
           //   },
           //   quantity: item.quantity,
-        };
-      }),
+        },
+      ],
+
       success_url: `${process.env.CLIENT_URL}/profile`,
       cancel_url: `${process.env.CLIENT_URL}/`,
     });
     res.json({ url: session.url });
   } catch (error) {
+    console.log('error');
     res.status(500).json({ error: error.message });
   }
   //   res.send("hi");
